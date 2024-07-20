da99a841a4867a46554aef79b21230171af22586
mabubakarkhan099 <164992239+mabubakarkhan099@users.noreply.github.com>
2024-07-20 10:59:55 +0500

    initialize repository

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
+import {  Urbanist } from "next/font/google";
 import "./globals.css";
+import "./sass/globals.scss"
+import Navbar from "@/components/major/Navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const urbanist = Urbanist({subsets :["latin"], weight: "400"})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={urbanist.className}>
+        <Navbar />
+        {children}
+        
+        </body>
     </html>
   );
 }
