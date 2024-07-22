e259700ccc9bef028e20b241cfd5fd0df0002da6
mabubakarkhan099 <164992239+mabubakarkhan099@users.noreply.github.com>
2024-07-22 10:20:45 +0500

    adde section projects

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Experience from "@/components/major/Experience/Experience";
 import HeroSection from "@/components/major/HeroSection/HeroSection";
+import HireMeSection from "@/components/major/HireMeSection/HireMeSection";
+import PortfolioProjects from "@/components/major/PortfolioProjects/PortfolioProjects";
 import ServicesSection from "@/components/major/ServicesSection/ServicesSection";
 import Image from "next/image";
 
@@ -9,6 +11,8 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <Experience />
+      <HireMeSection />
+      <PortfolioProjects />
     </>
   );
 }
